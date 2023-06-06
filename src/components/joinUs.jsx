332fd67c8732f13dcb6f08e77e940520284cbf67
Christopher Osuji <120677681+Orax101@users.noreply.github.com>
2023-06-06 14:10:50 +0100

    "Joinus..." under "about' was added as a component

---
+++ src/components/joinUs.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import  "./index.css";
+
+const makinAdiff=() => {
+  return(
+  <div>
+   <div className ="pt-[30px] pb-[30px] w-50 h-318 bg-[#04A667]">
+     <div>
+        <h2 className="mx-auto block mb-[10px] w-[436.78px] h-43 font-sans font-semibold text-2xl leading-6 tracking-wide text-white">Join Us in Making a Difference!</h2>
+        <p className="mx-auto block w-[985px] h-133 font-sans font-semibold text-xl leading-7 tracking-wide text-white">Together, we can transform the way we think about waste and create a more sustainable future. By participating in our platform, you contribute to the global effort to reduce plastic pollution while also benefiting from the rewards and opportunities offered by blockchain technology.</p>
+        <button className="mx-auto block mt-[10px] w-60 h-10 bg-white shadow-md rounded-xl"></button>
+    </div>
+    </div>
+   </div>
+  )
+}
+export default makinAdiff;
