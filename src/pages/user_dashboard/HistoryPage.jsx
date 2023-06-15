da90c29d2a36f53c86c844667470557dce8ec4f3
Absolute <106808580+chiemezie1@users.noreply.github.com>
2023-06-15 20:33:41 +0100

    Transactions tilte

--- src/pages/user_dashboard/HistoryPage.jsx
+++ src/pages/user_dashboard/HistoryPage.jsx
@@ -5,18 +5,16 @@ import UserDashboardLayout from "../../components/dashboard_components/UserDashb
 const HistoryPage = () => {
   return (
     <UserDashboardLayout active_link={'History'} dashboard_content={
-      <div className="w-3/4 bg-white h-full flex border-2 border-blue-200 ml-12">
-      <div className="w-3/4 p-4">
-        <h1 className=" text-3xl font-black text-primary40 py-4">History</h1>
-        <div className="m-4">
-          <div className="m-2">
-            
-            
-          </div>
-          <div className="m-2">
-            
-          </div>
+      <div className="w-3/4 bg-white h-full flex] ml-12">
+      {/* Transactions title */}
+        <div className="font-black text-primary40 p-4">
+        <h1 className="text-4xl font-bold text-primary40">History</h1>
+        <h3 className="text-2xl font-bold text-primary40 py-4">Transactions</h3>
         </div>
+
+        <div className="w-3/4 p-4">
+        
+
       </div>
     </div>
     } />
