7df989fa151f32ae9e5de01b065ddac07c9b1ff2
Absolute <106808580+chiemezie1@users.noreply.github.com>
2023-06-15 21:37:09 +0100

    added stateIcons

--- src/pages/user_dashboard/HistoryPage.jsx
+++ src/pages/user_dashboard/HistoryPage.jsx
@@ -12,9 +12,29 @@ const HistoryPage = () => {
         <h3 className="text-2xl font-bold text-primary40 py-4">Transactions</h3>
         </div>
         {/* Transactions container */}
-        <div className="m-4" style={{ background: 'linear-gradient(to right, rgba(138,227,119,1) 0%, rgba(193,247,181,1) 0%, rgba(255,255,255,1) 54%' }}>
-         {/* Your component's content */}
-         <p>sbhvs kxbfuykakv      ua vyyrawevur yvufhhaw v awhkjwaku wva aina\ic jniebfuybff hfbfbcub uwqioq ifncuebfu</p>
+        <div className="m-4 border-2 border-black" style={{ background: 'linear-gradient(to right, rgba(138,227,119,1) 0%, rgba(193,247,181,1) 0%, rgba(255,255,255,1) 54%' }}>
+         
+        <table>
+        <tr>
+          <th>TOTAL VALUE</th>
+          <th>TOTAL WEIGHT</th>
+          <th>TRANSACTION ID</th>
+          <th>ADDRESS</th>
+          <th>Header 3</th>
+        </tr>
+        <tr>
+          <td>55</td>
+          <td>100 KG</td>
+          <td>XXXX....XXX....XXX...XXX...</td>
+          <td> 001x33ff...</td>
+          <td>Row 2, Column 2</td>
+    
+        </tr>
+
+
+      </table>
+
+        fvhbverus uzbl
         </div>
     </div>
     } />
@@ -22,3 +42,29 @@ const HistoryPage = () => {
 };
 
 export default HistoryPage;
+
+
+
+
+
+
+
+
+
+// 5TH MAY 2020, 12:23
+// APPROVED
+// TOTAL VALUE
+// TOTAL WEIGHT
+// TRANSACTION ID
+// 55
+// 100 KG
+// 5TH MAY 2020, 12:23
+// CREDIT
+// ADDRESS
+// 5TH MAY 2020, 12:23
+// CREDIT
+// ADDRESS
+// 20 RC
+// 001x33ff...
+// 20 RC
+// 001x33ff...
