3cda6b2f766d9414a95f0046b5efa76e48977141
Absolute <106808580+chiemezie1@users.noreply.github.com>
2023-06-16 07:38:11 +0100

    style

--- src/pages/user_dashboard/HistoryPage.jsx
+++ src/pages/user_dashboard/HistoryPage.jsx
@@ -1,15 +1,16 @@
-import React from "react";
+import React, {useState} from "react";
 import Layout from "../../components/dashboard_components/UserDashboardLayout";
 import UserDashboardLayout from "../../components/dashboard_components/UserDashboardLayout";
 import stateGreen from "../../assets/stateGreen.svg";
 import stateRed from "../../assets/stateRed.svg";
 
 const HistoryPage = () => {
+  const [approve, satApprove] = useState("false");
   return (
     <UserDashboardLayout
       active_link={"History"}
       dashboard_content={
-        <div className="w-3/4 bg-white h-full flex] ml-12">
+        <div className="w-3/4 bg-white h-full ml-12">
           {/* Transactions title */}
           <div className="font-black text-primary40 p-10">
             <h1 className="text-4xl font-bold text-primary40">History</h1>
@@ -18,42 +19,38 @@ const HistoryPage = () => {
             </h3>
           </div>
           {/* Transactions container */}
-          <div
-            className="m-4 border-2 border-black"
-            style={{
-              background:
-                "linear-gradient(to right, rgba(138,227,119,1) 0%, rgba(193,247,181,1) 0%, rgba(255,255,255,1) 54%",
-            }}
-          >
-            <div className="flex flex-row  justify-between items-center">
-              <div className="flex flex-col items-center px-2">
+          <div className="m-4 border-2 border-black bg-gradient-to-r from-green-400 via-green-200 to-white">
+            <div className="flex flex-col md:flex-row justify-between items-center">
+              
+              <div className="flex flex-col items-center px-2 border-b-2 md:border-b-0 border-primary40 w-full">
                 <div className="font-semibold">TOTAL VALUE</div>
                 <div className="text-primary40 font-extrabold">55</div>
               </div>
 
-              <div className="flex flex-col items-center px-2">
+              <div className="flex flex-col items-center border-b-2 md:border-b-0 border-primary40 w-full">
                 <div className="font-semibold">TOTAL WEIGHT</div>
                 <div className="text-primary40 font-extrabold">100 KG</div>
               </div>
 
-              <div className="flex flex-col items-center px-2">
+              <div className="flex flex-col items-center px-2 border-b-2 md:border-b-0 border-primary40 w-full">
                 <div className="font-semibold">TRANSACTION ID</div>
                 <div className="text-primary40 font-extrabold">XXXX....XXX....XXX...XXX...</div>
               </div>
 
-              <div className="flex flex-col items-center px-2">
+              <div className="flex flex-col items-center border-b-2 md:border-b-0 border-primary40 w-full">
                 <div className="font-semibold">ADDRESS</div>
                 <div className="text-primary40 font-extrabold">001x33ff...</div>
               </div>
 
               <div className="flex flex-col items-end m-2">
-                  <img className="h-7 w-7 bg-red-700 m-2 p-2" src={stateRed} alt="State Icon Green" />
-                  <div className="text-xs">5TH MAY 2020, 12:23</div>
-                  <div className="text-right justify-end text-primary40 font-extrabold">APPROVED</div>
+                <img className="h-7 w-7 bg-red-700 m-2 p-2 hidden md:block" src={stateRed} alt="State Icon Green" />
+                <div className="text-xs font-bold text-gray-500">5TH MAY 2020, 12:23</div>
+                <div className="text-right text-primary40 font-extrabold">APPROVED</div>
               </div>
-
             </div>
           </div>
+
+          
         </div>
       }
     />
@@ -61,21 +58,3 @@ const HistoryPage = () => {
 };
 
 export default HistoryPage;
-
-// 5TH MAY 2020, 12:23
-// APPROVED
-// TOTAL VALUE
-// TOTAL WEIGHT
-// TRANSACTION ID
-// 55
-// 100 KG
-// 5TH MAY 2020, 12:23
-// CREDIT
-// ADDRESS
-// 5TH MAY 2020, 12:23
-// CREDIT
-// ADDRESS
-// 20 RC
-// 001x33ff...
-// 20 RC
-// 001x33ff...
