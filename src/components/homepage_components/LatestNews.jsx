e8e204448c31e68b4594ff8e1f3a888a0aa8d22b
Surulere Chris. I. Abiye <suruabiye@gmail.com>
2023-06-16 13:42:02 +0100

    update news section

--- src/components/homepage_components/LatestNews.jsx
+++ src/components/homepage_components/LatestNews.jsx
@@ -1,161 +1,48 @@
 import React from 'react'
 
+// Import images
+import clean_image from "../../assets/clean-image-1.svg";
+import news_2 from "../../assets/news-2.svg";
+import news_3 from "../../assets/news-3.svg";
+
 const LatestNews = () => {
-  return (
-      <>
-        {/* Start Latest News.. */}
-        <div className="container my-24 px-6 mx-auto">
-          {/* Section: Design Block */}
-          <section className="mb-32 text-gray-800 text-center">
-            <h2 className="text-[#0D4D00] uppercase text-2xl font-extrabold mb-12 pb-4 text-center">
-              Latest News
-            </h2>
-            <div className="grid lg:grid-cols-3 gap-6 xl:gap-x-12">
-              <div className="mb-6 lg:mb-0">
-                <div className="relative block bg-white rounded-lg shadow-lg">
-                  <div className="flex">
-                    <div
-                        className="  relative overflow-hidden bg-no-repeat bg-cover shadow-lg rounded-lg mx-4 -mt-4"
-                        data-mdb-ripple="true"
-                        data-mdb-ripple-color="light"
-                    >
-                      <img
-                          src="https://mdbcdn.b-cdn.net/img/new/standard/city/024.webp"
-                          className="w-full"
-                      />
-                      <a href="#!">
-                        <div
-                            className="absolute top-0 right-0 bottom-0 left-0 w-full h-full overflow-hidden bg-fixed opacity-0 hover:opacity-100 transition duration-300 ease-in-out"
-                            style={{ backgroundColor: "rgba(251, 251, 251, 0.15)" }}
-                        />
-                      </a>
-                    </div>
-                  </div>
-                  <div className="p-6">
-                    <h5 className="font-bold text-lg mb-3">My paradise</h5>
-                    <p className="text-gray-500 mb-4">
-                      <small>
-                        Published <u>13.01.2022</u> by
-                        <a href="" className="text-gray-900 p-1">
-                          Chris
-                        </a>
-                      </small>
-                    </p>
-                    <p className="mb-4 pb-2">
-                      Ut pretium ultricies dignissim. Sed sit amet mi eget urna
-                      placerat vulputate. Ut vulputate est non quam dignissim
-                      elementum. Donec a ullamcorper diam.
-                    </p>
-                    <a
-                        href="#!"
-                        data-mdb-ripple="true"
-                        data-mdb-ripple-color="light"
-                        className="inline-block px-6 py-2.5 bg-primary40 text-white font-medium text-xs leading-tight capitalize rounded-full shadow-md transition duration-150 ease-in-out"
-                    >
-                      Read more
-                    </a>
-                  </div>
-                </div>
-              </div>
-              <div className="mb-6 lg:mb-0">
-                <div className="relative block bg-white rounded-lg shadow-lg">
-                  <div className="flex">
-                    <div
-                        className=" relative overflow-hidden bg-no-repeat bg-cover shadow-lg rounded-lg mx-4 -mt-4"
-                        data-mdb-ripple="true"
-                        data-mdb-ripple-color="light"
-                    >
-                      <img
-                          src="https://mdbcdn.b-cdn.net/img/new/standard/city/031.webp"
-                          className="w-full"
-                      />
-                      <a href="#!">
-                        <div
-                            className="absolute top-0 right-0 bottom-0 left-0 w-full h-full overflow-hidden bg-fixed opacity-0 hover:opacity-100 transition duration-300 ease-in-out"
-                            style={{ backgroundColor: "rgba(251, 251, 251, 0.15)" }}
-                        />
-                      </a>
-                    </div>
-                  </div>
-                  <div className="p-6">
-                    <h5 className="font-bold text-lg mb-3">Travel to Italy</h5>
-                    <p className="text-gray-500 mb-4">
-                      <small>
-                        Published <u>12.01.2022</u> by
-                        <a href="" className="text-gray-900 p-1">
-                          Jane Doe
-                        </a>
-                      </small>
-                    </p>
-                    <p className="mb-4 pb-2">
-                      Suspendisse in volutpat massa. Nulla facilisi. Sed aliquet diam
-                      orci, nec ornare metus semper sed. Integer volutpat ornare erat
-                      sit amet rutrum.
-                    </p>
-                    <a
-                        href="#!"
-                        data-mdb-ripple="true"
-                        data-mdb-ripple-color="light"
-                        className="inline-block px-6 py-2.5 bg-primary40 text-white font-medium text-xs leading-tight capitalize rounded-full shadow-md transition duration-150 ease-in-out"
-                    >
-                      Read more
-                    </a>
-                  </div>
-                </div>
-              </div>
-              <div className="mb-0">
-                <div className="relative block bg-white rounded-lg shadow-lg">
-                  <div className="flex">
-                    <div
-                        className=" relative overflow-hidden bg-no-repeat bg-cover shadow-lg rounded-lg mx-4 -mt-4"
-                        data-mdb-ripple="true"
-                        data-mdb-ripple-color="light"
-                    >
-                      <img
-                          src="https://mdbcdn.b-cdn.net/img/new/standard/city/081.webp"
-                          className="w-full"
-                      />
-                      <a href="#!">
-                        <div
-                            className="absolute top-0 right-0 bottom-0 left-0 w-full h-full overflow-hidden bg-fixed opacity-0 hover:opacity-100 transition duration-300 ease-in-out"
-                            style={{ backgroundColor: "rgba(251, 251, 251, 0.15)" }}
-                        />
-                      </a>
-                    </div>
-                  </div>
-                  <div className="p-6">
-                    <h5 className="font-bold text-lg mb-3">Chasing the sun</h5>
-                    <p className="text-gray-500 mb-4">
-                      <small>
-                        Published <u>10.01.2022</u> by
-                        <a href="" className="text-gray-900 p-1">
-                          John Doe
-                        </a>
-                      </small>
-                    </p>
-                    <p className="mb-4 pb-2">
-                      Curabitur tristique, mi a mollis sagittis, metus felis mattis
-                      arcu, non vehicula nisl dui quis diam. Mauris ut risus eget
-                      massa volutpat feugiat. Donec.
-                    </p>
-                    <a
-                        href="#!"
-                        data-mdb-ripple="true"
-                        data-mdb-ripple-color="light"
-                        className="inline-block px-6 py-2.5 bg-primary40 text-white font-medium text-xs leading-tight capitalize rounded-full shadow-md transition duration-150 ease-in-out"
-                    >
-                      Read more
-                    </a>
-                  </div>
-                </div>
-              </div>
+  const newsData = [
+    {
+      title: 'Plastics recycling market set to grow big',
+      summary: 'According to recent market research data, the plastics recycling market \n' +
+          'industry, is poised for major growth within the next decade.',
+      imageUrl:  clean_image,
+    },
+    {
+      title: 'The Winners of the Plastics Recycling Awards Europe 2023',
+      imageUrl: news_2,
+    },
+    {
+      title: 'Germany will force companies to pay for cleaning up single use plastic',
+      imageUrl: news_3,
+    },
+  ];
+  return <>
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+      <div className="col-span-1 sm:col-span-2 md:col-span-2 bg-white p-4">
+        <img src={newsData[0].imageUrl} alt={`News 1`} className="w-full rounded-2xl mb-4" />
+        <h3 className="text-[26px] font-[open-sans] font-extrabold">{newsData[0].title}</h3>
+        <p className="font-[open-sans] text-black-600 font-semibold text-[16]">{newsData[0].summary}</p>
+        <p className="font-[open-sans] text-black-600 font-semibold text-[16] py-4 mr-8">
+          <span className="mr-10"><i className=""></i>May 20, 2023</span>
+          <span className="mr-10">Reading Time: about 3 minutes</span>
+        </p>
+      </div>
+      <div className="sm:col-span-2 md:col-span-1">
+        {newsData.slice(1).map((news, index) => (
+            <div key={index} className="bg-white p-4 flex flex-col">
+              <img src={news.imageUrl} alt={`News ${index + 2}`} className="w-full rounded-2xl mb-4" />
+              <h3 className="text-lg font-extrabold font-[open-sans]">{news.title}</h3>
             </div>
-          </section>
-          {/* Section: Design Block */}
-        </div>
-        {/* End latest News */}
-      </>
-  )
+        ))}
+      </div>
+    </div>
+  </>
 }
 
 export default LatestNews
