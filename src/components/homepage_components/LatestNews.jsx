bdf7d7b4a9a3724bbb99d5ed630747af49c938ab
Surulere Chris. I. Abiye <suruabiye@gmail.com>
2023-06-16 13:47:22 +0100

    update the title

--- src/components/homepage_components/LatestNews.jsx
+++ src/components/homepage_components/LatestNews.jsx
@@ -23,25 +23,36 @@ const LatestNews = () => {
     },
   ];
   return <>
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
-      <div className="col-span-1 sm:col-span-2 md:col-span-2 bg-white p-4">
-        <img src={newsData[0].imageUrl} alt={`News 1`} className="w-full rounded-2xl mb-4" />
-        <h3 className="text-[26px] font-[open-sans] font-extrabold">{newsData[0].title}</h3>
-        <p className="font-[open-sans] text-black-600 font-semibold text-[16]">{newsData[0].summary}</p>
-        <p className="font-[open-sans] text-black-600 font-semibold text-[16] py-4 mr-8">
-          <span className="mr-10"><i className=""></i>May 20, 2023</span>
-          <span className="mr-10">Reading Time: about 3 minutes</span>
-        </p>
-      </div>
-      <div className="sm:col-span-2 md:col-span-1">
-        {newsData.slice(1).map((news, index) => (
-            <div key={index} className="bg-white p-4 flex flex-col">
-              <img src={news.imageUrl} alt={`News ${index + 2}`} className="w-full rounded-2xl mb-4" />
-              <h3 className="text-lg font-extrabold font-[open-sans]">{news.title}</h3>
-            </div>
-        ))}
-      </div>
+    {/* Start Latest News.. */}
+    <div className="container my-24 px-6 mx-auto">
+      {/* Section: Design Block */}
+      <section className="mb-32 text-gray-800 text-center">
+        <h2 className="text-[#0D4D00] uppercase text-[26px] font-extrabold mb-12 pb-4 text-center">
+          Latest News
+        </h2>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+          <div className="col-span-1 sm:col-span-2 md:col-span-2 bg-white p-4">
+            <img src={newsData[0].imageUrl} alt={`News 1`} className="w-full rounded-2xl mb-4" />
+            <h3 className="text-[26px] font-[open-sans] font-extrabold">{newsData[0].title}</h3>
+            <p className="font-[open-sans] text-black-600 font-semibold text-[16]">{newsData[0].summary}</p>
+            <p className="font-[open-sans] text-black-600 font-semibold text-[16] py-4 mr-8">
+              <span className="mr-10"><i className=""></i>May 20, 2023</span>
+              <span className="mr-10">Reading Time: about 3 minutes</span>
+            </p>
+          </div>
+          <div className="sm:col-span-2 md:col-span-1">
+            {newsData.slice(1).map((news, index) => (
+                <div key={index} className="bg-white p-4 flex flex-col">
+                  <img src={news.imageUrl} alt={`News ${index + 2}`} className="w-full rounded-2xl mb-4" />
+                  <h3 className="text-lg font-extrabold font-[open-sans]">{news.title}</h3>
+                </div>
+            ))}
+          </div>
+        </div>
+      </section>
+      {/* Section: Design Block */}
     </div>
+    {/* End latest News */}
   </>
 }
 
