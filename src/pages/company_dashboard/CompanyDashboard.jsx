fce948891decda1d734f77155c057988d89d7d3b
CyberGA <etomu.joshua@gmail.com>
2023-06-20 14:17:47 +0100

    company transaction done

--- src/pages/company_dashboard/CompanyDashboard.jsx
+++ src/pages/company_dashboard/CompanyDashboard.jsx
@@ -1,12 +1,8 @@
 import CompanyDashboardLayout from "../../components/dashboard_components/CompanyDashboardLayout"
-import CompanyDashboardComponent from "./CompanyDashboardComponent"
+import CompanyTransactions from './../../components/company_transactions/index';
 
 const CompanyDashboard = () => {
-  return (
-    <CompanyDashboardLayout active_link={"Dashboard"} dashboard_content={
-      <CompanyDashboardComponent/>
-    } />
-  )
+  return <CompanyDashboardLayout active_link={"Dashboard"} dashboard_content={<CompanyTransactions />} />;
 }
 
 export default CompanyDashboard
